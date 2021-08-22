f7e02b061dd13fea86120c3a676a5f2db9ca7a20
Solene D <solene98@student.ubc.ca>
2021-08-22 16:26:43 +0000

    add seeds for favorites and messages tables

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -1,4 +1,5 @@
--- note: should we insert the user id??
+-- note: should we manually insert the ids ??
+
 
 
 -- Seeds for users table
@@ -34,3 +35,37 @@ VALUES (04, 'handbag', 'Original price: $150', 70, '../../public/images/product_
 
 INSERT INTO products (id, name, description, price, image_path)
 VALUES (05, 'laptop', 'Selling brand new laptop, 15 inch screen', 999, '../../public/images/product_images/laptop.jpg');
+
+
+
+
+-- Seeds for favorites table
+
+INSERT INTO favorites (id, product_id, user_id)
+VALUES (382, 03, 1);
+
+
+INSERT INTO favorites (id, product_id, user_id)
+VALUES (041, 02, 1);
+
+INSERT INTO favorites (id, product_id, user_id)
+VALUES (901, 05, 3);
+
+
+
+-- Seeds for messages table
+
+INSERT INTO messages (id, product_id, user_id, is_sold, datetime, content)
+VALUES (14, 05, 3, FALSE, '2020-03-22 04:05:06', 'How old is the laptop?');
+
+
+INSERT INTO messages (id, product_id, user_id, is_sold, datetime, content)
+VALUES (22, 03, 1, FALSE, '2019-06-04 10:43:20', 'hello, I am interested in the coffee');
+
+-- message from an admin (?)
+INSERT INTO messages (id, product_id, user_id, is_sold, datetime, content)
+VALUES (13, 03, 1, TRUE, '2019-06-04 11:20:07', 'Sorry, the coffee is sold out');
+
+
+
+

5628c8d46f05e5ea387e8f0eea6dd891d15ed5de
Solene D <solene98@student.ubc.ca>
2021-08-22 15:12:20 +0000

    create seeds for users table

---
+++ db/seeds/01_seeds.sql
@@ -0,0 +1,17 @@
+
+-- Seeds for users table
+
+INSERT INTO users (name, phone, country, province, city, street, postal_code)
+VALUES ('User1', 111111111, 'Canada', 'British Columbia', 'Street 1', '111 111');
+
+INSERT INTO users (name, phone, country, province, city, street, postal_code)
+VALUES ('User2', 222222222, 'Canada', 'Ontario', 'Street 2', '222 222');
+
+INSERT INTO users (name, phone, country, province, city, street, postal_code)
+VALUES ('User3', 333333333, 'Canada', 'Quebec', 'Street 3', '333 333');
+
+
+
+
+
+

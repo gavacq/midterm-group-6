b7f9b1eb0c62ae68366537f53c39a0e01be292e3
Solene D <solene98@student.ubc.ca>
2021-08-24 18:42:27 +0000

    install cookie-session

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ const PORT       = process.env.PORT || 8080;
 const ENV        = process.env.ENV || "development";
 const express    = require("express");
 const app        = express();
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 const sass       = require("node-sass-middleware");
 const morgan     = require('morgan');
+const cookieSession = require('cookie-session');
+
+
+//
 
 // PG database client/connection setup
 const { Pool } = require('pg');
@@ -22,9 +26,10 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
   dest: __dirname + "/public/styles",
@@ -33,6 +38,14 @@ app.use("/styles", sass({
 }));
 
 app.use(express.static("public"));
+
+// ---------------- SET UP COOKIES ----------------------- //
+
+// All cookies (id, email, password) are stored in 'session'
+app.use(cookieSession({
+  name: 'session',
+  keys: ['ksdjygfti7436r5ov8q47tv','ksufby3iyfvbpq398y']
+}));
 
 
 // ------------------------- SET UP ROUTING & HELPERS ---------------------------------- //

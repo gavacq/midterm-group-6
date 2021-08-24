40b14db3ddaa30240125eea92b043ee0a4f70ab9
geecrypt <gavina352@gmail.com>
2021-08-23 19:05:58 -0700

    add more components

---
+++ public/scripts/network.js
@@ -0,0 +1,16 @@
+function getProducts(params) {
+  let url = "/products";
+  if (params) {
+    url += "?" + params;
+  }
+  
+  return $.ajax({url, });
+}
+
+const addProduct = function(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/products",
+    data,
+  });
+};

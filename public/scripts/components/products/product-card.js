ea547c3ec592a647a4c83c84bd4b63e283d20a4f
Solene D <solene98@student.ubc.ca>
2021-08-25 01:56:46 +0000

    lint product-card.js and update comments

--- public/scripts/components/products/product-card.js
+++ public/scripts/components/products/product-card.js
@@ -1,12 +1,13 @@
-// NOTES: 
-//  LINE 18 - 19 ONLY IF ADMIN: show if the item is in the favorites
+// NOTES:
+//  LINE 18 - 19 ONLY IF ADMIN: show if the item is in the favorites - render the heart icon here?
 // NOTE: in the template strings should the variable names be req.body or product.etc ??
 
 $(document).ready(function() {
 
-  // FUNCTION: generates the HTML for the modal view of a product
+  // FUNCTION: generates the HTML for the modal view of a product.
+  // if admin, the favorites part should not be generated.
+  
   const createProductCard = () => {
-
     return `
     <article class = 'product-card'>
       <section class = 'product-card-preview-image'>
@@ -18,17 +19,10 @@ $(document).ready(function() {
           <li>Price: ${product.price}</li>
           <li>Description: ${product.description}</li>
         </ul>
-        ${product.favorite ? 
-          `This product is in your favorites (render heart icon?)` : ``}
+        ${product.favorite ?
+    `This product is in your favorites` : ``}
       </section> 
     </article>
-    `
-  }
-}); 
-
-  
-
-
-
-
-
+    `;
+  };
+});

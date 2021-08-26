8ab46e57a8ee300da602deccb667d2f73e086ccf
woodpeckershop <chris.sun5567@gmail.com>
2021-08-25 19:37:55 -0700

    create chat-list content

---
+++ public/scripts/components/chats/chat-card.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // add empty object to AppLib library
+  AppLib.chatCard = {};
+
+  // FUNCTION: generates the HTML for the modal view of a product.
+  const createChatCard = chat => {
+    return `
+
+    `;
+  };
+
+    // add the createChatCard method to the chatCard object in the AppLib library
+    AppLib.chatCard.createChatCard = createChatCard;
+});
--- public/scripts/components/chats/chat-list.js
+++ public/scripts/components/chats/chat-list.js
@@ -7,4 +7,18 @@ $(() => {
 
   AppLib.$chatList = $chatList;
   AppLib.chatList = {};
+
+  function addChatCard(chatCard) {
+    $chatList.append(chatCard);
+  }
+
+  function createChatList(chats) {
+    for (const chatId in chats) {
+      const chat = chats[chatId];
+      const chatCard = AppLib.chatCard.createChatCard(chat);
+      addChatCard(chatCard);
+    }
+  }
+  
+  AppLib.chatList.createChatList = createChatList;
 });

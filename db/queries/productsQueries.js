ac44bbd265e4a316eddc167b5c61cbac4cdd9dfc
woodpeckershop <chris.sun5567@gmail.com>
2021-08-26 11:12:47 -0700

    create delete fav post req and query function

--- db/queries/productsQueries.js
+++ db/queries/productsQueries.js
@@ -93,8 +93,7 @@ module.exports = db => {
     // USER: add a product to favorites
     addToFavorites(product, userId) {
       const queryString = `INSERT INTO favorites (product_id, user_id)
-                           WHERE product_id = $1
-                           AND user_id = $2`;
+      VALUES ($1, $2);`
 
       const queryParams = [product.productId, userId];
 
@@ -104,18 +103,17 @@ module.exports = db => {
         .catch(error => error.message);
     },
 
-    deleteFavourites(product, userId){
-      const queryString = `DELETE FROM favorites (product_id, user_id)
-      WHERE product_id = $1
-      AND user_id = $2`;
+    deleteFavourites(product, userId) {
+      const queryString = `DELETE FROM favorites
+      WHERE product_id = $1 AND user_id = $2`;
 
       const queryParams = [product.productId, userId];
 
       return db
-      .query(queryString, queryParams)
-      .then(result => console.log('product was deleted from favorites.'))
-      .catch(error => error.message);
-    }
+        .query(queryString, queryParams)
+        .then(result => console.log('product was deleted from favorites.'))
+        .catch(error => error.message);
+    },
 
     // ADMIN: delete a product
     deleteProduct(product) {
--- routes/products.js
+++ routes/products.js
@@ -56,7 +56,7 @@ module.exports = productsQueries => {
     .route('/:productId')
     .get((req, res) => {
       const product = {productId: req.params.productId};
-
+      console.log(req.params);
       // user or admin views a product modal
       productsQueries.viewProduct(product)
         .then(data => {
@@ -74,6 +74,25 @@ module.exports = productsQueries => {
 
         // user adds a product to favorites
         productsQueries.addToFavorites(product, req.session.userId)
+          .then(data => {
+            res.json(data);
+          })
+          .catch(err => console.log(err));
+      }
+    });
+
+  router
+    .route('/:productId/fav')
+    .post((req, res) => {
+      // check that user is NOT admin
+     
+      if (req.session.userId !== adminId) {
+        const product = {
+          productId: req.params.productId,
+        };
+        console.log(product);
+        // user adds a product to favorites
+        productsQueries.deleteFavourites(product, req.session.userId)
           .then(data => {
             res.json(data);
           })

1febb527ad37bdb662381da296b9ef7266e74d87
geecrypt <gavina352@gmail.com>
2021-08-27 06:28:58 -0700

    click logo to go home

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,5 +4,9 @@ $(() => {
   $('.fas.fa-comment-dots').on('click', ()=>{
     AppLib.showChatsView();
   });
+
+  $('#logo').on('click', () => {
+    AppLib.showProductsView();
+  });
   // add favorites filter button here
 });
